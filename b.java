3fae033d6cf296468c5499e9128dec9ae7fcef4c
balajisuryavanshi <159866189+balajisuryavanshi@users.noreply.github.com>
2024-08-12 15:25:44 +0530

    b.java

---
+++ b.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class b
+{
+public static void main(String args[])
+{
+int a=-10;
+if(a==0)
+{
+System.out.println("A is greater than 0");
+}
+else if(a<0)
+{
+System.out.println("number is negative");
+}
+else
+{
+System.out.println("number is positive");
+}
+}
+}
