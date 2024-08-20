5bd61ef6fd7182b24873f59527d7e0cab735df9f
balajisuryavanshi <159866189+balajisuryavanshi@users.noreply.github.com>
2024-08-20 19:48:33 +0530

    Create do_while_loop.java

---
+++ do_while_loop.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+class do_while_loop
+{
+    public static void main(String args[])
+{
+    int x;
+    x=1;
+    do{
+        System.out.println(x);
+        x++;
+    }
+    while(x<=5);
+}
+}
